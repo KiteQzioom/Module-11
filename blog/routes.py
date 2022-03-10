798734cf8db78d8aee8ea517e0d80c3bf2e4bcaf
KiteQzioom <93146769+KiteQzioom@users.noreply.github.com>
2022-03-10 15:44:43 +0100

    11.3

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,22 @@
-from flask import render_template, request
+from flask import render_template, request, session, flash, redirect, url_for
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
+
+
+@app.route("/drafts/", methods=['GET'])
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
 
 @app.route("/")
 def index():
@@ -36,4 +51,33 @@ def post(entry_id):
                    db.session.commit()
             else:
                  errors = form.errors
-        return render_template("entry_form.html", form=form, errors=errors)    
+        return render_template("entry_form.html", form=form, errors=errors)   
+
+@app.route("/delete/<int:entry_id>", methods=["POST"]) 
+def delete(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    db.session.delete(entry)
+    db.session.commit()
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
